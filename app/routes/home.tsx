65aab901d522ebae9e7484954145a8f1ccacd2f0
Esraa Abduallah <esraahisham753@gmail.com>
2025-08-05 19:03:15 +0300

    finalize the app

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -59,8 +59,8 @@ export default function Home() {
           {
               !loading && resumes.length === 0 && (
                   <div className="flex flex-col items-center justify-center mt-10 gap-4">
-                      <Link to="/upload" className="primaryButton w-fit text-xl font-semibold">
-                          Upload
+                      <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+                          Upload Resume
                       </Link>
                   </div>
               )
